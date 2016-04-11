eef7905c8d918b2dc18e7637012792a44d6dc322
anshuman172 <anshuman172@gmail.com>
2016-04-11 20:26:59 +0530

    Assignment1

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+package session3;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+
+		int a = 2;
+		System.out.println("Square Root : " + Math.sqrt(a));
+		System.out.println("Cube Root : " + Math.cbrt(a));
+
+	}
+
+}
